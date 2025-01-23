fec3554aab0a5b9d102871672c8c6bbddb2d3805
SergeWilfried <63460822+SergeWilfried@users.noreply.github.com>
2025-01-23 11:47:12 +0000

    Update siteConfig.ts

--- src/app/siteConfig.ts
+++ src/app/siteConfig.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  url: "https://solar.tremor.so",
+  url: "https://solar.bangre.co",
   name: "Solar Technologies - Smart Farming Solutions",
   description: "AI-powered solutions for sustainable, efficient, and profitable farming. Autonomous monitoring, precision agriculture, smart irrigation, and more.",
   baseLinks: {
